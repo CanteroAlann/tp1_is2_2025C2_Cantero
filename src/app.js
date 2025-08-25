726e969122cf8657e7d832939b8b9526aa64ed86
alan <acantero@fi.uba.ar>
2025-08-25 18:25:57 -0300

    feat : some changes to support db for tests

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express, { json } from "express";
-import { connect } from "mongoose";
+import connectDB from "./utils/db.js";
 import { MONGO_URI, ENV } from "./utils/config.js";
 import { setupLogger } from "./utils/logger.js";
 import { errorHandler, mongoErrorHandler } from "./utils/middleware.js";
 
 import cors from "cors";
 
+connectDB();
+
 const app = express();
 
 app.use(cors());
 app.use(json());
-connect(MONGO_URI);
+
 
 import songsRouter from "./routes/songs.js";
 import playlistsRouter from "./routes/playlists.js";
