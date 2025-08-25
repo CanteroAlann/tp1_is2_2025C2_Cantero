fe4b96408dce7e92ac4aa4505821498019d5aa38
alan <acantero@fi.uba.ar>
2025-08-25 18:25:00 -0300

    feat : add enviroments variables for tsts'sdb

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,19 @@ import { config } from "dotenv";
 
 const result = config();
 
-let MONGO_URI = process.env.MONGO_URI;
+
 let PORT = process.env.PORT || 8080;
-let ENV = process.env.ENVIRONMENT || "development";
+let ENV = process.env.NODE_ENV || "development";
+
+
+let MONGO_URI = "";
+
+if (ENV === "development") {
+  MONGO_URI = process.env.MONGO_URI_DEV;
+} else if (ENV === "test") {
+  MONGO_URI = process.env.MONGO_URI_TEST;
+} else if (ENV === "production") {
+  MONGO_URI = process.env.MONGO_URI_PROD;
+}
 
 export { MONGO_URI, PORT, ENV };
